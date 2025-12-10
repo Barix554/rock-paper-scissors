62d69a20274fbb9cf1b6b14624326db6d4783d53
Bartosz Kowalski <bartekkowal200@gmail.com>
2025-12-10 20:32:10 +0100

    Add input.txt and update Main.java for correct file path

--- Rock_Paper_Scissors/src/main/java/org/example/Main.java
+++ Rock_Paper_Scissors/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int total = RockPaperScissors.calculateFromFile("Rock_Paper_Scissors/input.txt");
+        int total = RockPaperScissors.calculateFromFile("input.txt");
         System.out.println("Total score: " + total);
     }
 }
